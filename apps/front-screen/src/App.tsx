0679721ad6f45545f85918c8b94a1db886c3e3c0
Arthur <arthurjenckdev@gmail.com>
2026-03-15 12:27:14 +0100

    feat/debug: added leva gui

--- apps/front-screen/src/App.tsx
+++ apps/front-screen/src/App.tsx
@@ -1,16 +1,26 @@
+import type { CameraProps } from "@react-three/fiber"
 import { Physics } from "@react-three/rapier"
+import { Leva } from "leva"
+import DebugCamera from "./components/DebugCamera"
 import Walls from "./components/Walls"
 import World from "./components/World"
 
+const isDebug = import.meta.env.MODE === "development"
+
 export default function App() {
+  const cameraSettings = { position: [0, 20, 25] as [number, number, number], fov: 35 }
+
   return (
     <>
-      {/* <WebsocketTest /> */}
-      <World cameraSettings={{ position: [0, 20, 25], fov: 35 }}>
+      <Leva
+        hidden={!isDebug}
+        titleBar={{ title: "Tweaks GUI" }}
+        theme={{ sizes: { rootWidth: "350px" } }}
+      />
+      <World cameraSettings={cameraSettings as CameraProps}>
         <ambientLight intensity={0.5} />
         <directionalLight position={[5, 5, 5]} intensity={1} />
-        {/* Debug only - will be toggleable with Leva GUI */}
-        {/* <OrbitControls /> */}
+        <DebugCamera cameraPosition={cameraSettings.position} cameraFov={cameraSettings.fov} />
 
         <Physics debug>
           <Walls />
