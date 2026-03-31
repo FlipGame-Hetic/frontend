7b14bea1ce5d3ad853e110b4025d77ee7892edc9
Jeck0v <arnaud.fischer75@gmail.com>
2026-03-31 04:10:04 +0200

    Fixing Github Page error

--- apps/back-screen/vite.config.ts
+++ apps/back-screen/vite.config.ts
@@ -4,7 +4,6 @@ import tailwindcss from "@tailwindcss/vite"
 import { resolve } from "node:path"
 
 export default defineConfig({
-  base: "./",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
