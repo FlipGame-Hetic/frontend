161eaa10b45d35754b0874b68f3869cdf4efccc2
Arthur <arthurjenckdev@gmail.com>
2026-06-17 11:56:11 +0200

    improv/configs: tweaked bonus zone and screen shake

--- apps/front-screen/src/components/screenShake/screenShakeConfig.ts
+++ apps/front-screen/src/components/screenShake/screenShakeConfig.ts
@@ -1,7 +1,7 @@
 export const TRAUMA_DECAY = 1
-export const MAX_OFFSET = 0.1
-export const MAX_OFFSET_Z = 0.1
-export const MAX_OFFSET_Y = 0.1
+export const MAX_OFFSET = 0.07
+export const MAX_OFFSET_Z = 0.07
+export const MAX_OFFSET_Y = 0.07
 
 export const SHAKE_INTENSITY = {
   bumper: 0.2,
