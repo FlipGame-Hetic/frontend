44abcc204c926383d268881b6c91f2b380d630f2
Arthur <arthurjenckdev@gmail.com>
2026-06-30 12:59:48 +0200

    fix/controls-backScreen: made hints correct with cabinet

--- apps/back-screen/src/components/MenuControlsLegend.tsx
+++ apps/back-screen/src/components/MenuControlsLegend.tsx
@@ -5,13 +5,13 @@ export function MenuControlsLegend() {
   return (
     <>
       <div className="absolute bottom-10 left-12 z-20 flex flex-col gap-3">
-        <ControlHint control={MENU_CONTROLS.navigateLeft} side="left" />
         <ControlHint control={MENU_CONTROLS.back} side="left" />
+        <ControlHint control={MENU_CONTROLS.navigateLeft} side="left" />
       </div>
 
       <div className="absolute right-12 bottom-10 z-20 flex flex-col gap-3">
-        <ControlHint control={MENU_CONTROLS.navigateRight} side="right" />
         <ControlHint control={MENU_CONTROLS.confirm} side="right" />
+        <ControlHint control={MENU_CONTROLS.navigateRight} side="right" />
       </div>
     </>
   )
